897c2d9de8fb3eae65420051916f059854b2f0d0
mishaneu <doctorfomincov2005@mail.ru>
2026-04-14 15:12:11 +0300

    feat: create a base room, lighting and environment

--- Unity Project/OnlineSchool/Assets/Scripts/Player/PlayerMovement.cs
+++ Unity Project/OnlineSchool/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,7 +9,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
